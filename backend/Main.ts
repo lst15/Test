16b0ff0348f5d3bf00a4215c46d7cb6955d554d6
lst15 <leandro.salustriano@hotmail.com>
2025-03-31 17:45:51 -0300

    Add TypeScript and development dependencies setup

--- backend/Main.ts
+++ backend/Main.ts
@@ -1,3 +1,15 @@
-(() => {
-    //init
-})
+import {Mongo} from "./src/configs/MongoConnect";
+import {Properties} from "./src/configs/Properties";
+import {UserCtrl} from "./src/controllers/UserCtrl";
+import {ExpressServer} from "./src/controllers/ExpressServer";
+import {UserFactory} from "./src/factories/UserFactory";
+
+(async () => {
+    Properties.initialize()
+    UserFactory.loadRepository()
+
+    await Mongo.connect()
+
+    new UserCtrl();
+    ExpressServer.initialize()
+})()
